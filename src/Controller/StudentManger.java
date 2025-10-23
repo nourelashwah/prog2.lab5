88560c3ca06f4a96deca86a07c9818a0e64b7937
nourelashwah <elashwahnour@gmail.com>
2025-10-24 01:40:35 +0300

    featur 2 added search student method

---
+++ src/Controller/StudentManger.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LapTop
+ */
+public class StudentManger {
+    private ArrayList<Student> students;
+
+    public StudentManger() {
+         students= new ArrayList<>();
+         
+    }
+    public void addStudent
+    
+    
+}
