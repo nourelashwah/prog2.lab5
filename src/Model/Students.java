46d44581ca1aaf5139e39ab0584574576945105e
haneenusama20 <haneenusama101@gmail.com>
2025-10-24 12:58:01 +0300

    Updated StudentsClass after abstract

--- src/Model/Students.java
+++ src/Model/Students.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package Model;
 
 /**
  *
@@ -12,8 +12,8 @@ package model;
 public class Students extends Person {
     
     
-    public Students(int personID,String fullName,int age,String gender,String department,float gpa) {
-        super(personID, fullName, age, gender, department, gpa); 
+    public Students(int id,String fullName,int age,String gender,String department,float gpa) {
+        super(id, fullName, age, gender, department, gpa); 
     }
      
     
