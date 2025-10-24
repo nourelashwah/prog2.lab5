a86bc194438f116f8f02c8b5f746d25042a1e707
kithsope <malakhassanabdelhamid@gmail.com>
2025-10-24 17:09:36 +0300

    added create student helper method for file loading/reading

--- src/Model/FileManager.java
+++ src/Model/FileManager.java
@@ -4,10 +4,40 @@
  */
 package Model;
 
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Student;
+
 /**
  *
  * @author malak
  */
 public class FileManager {
+    private String filename;
+    private ArrayList<Student> students;
+
+    public FileManager(String filename, ArrayList<Student> students) {
+        this.filename = filename;
+        this.students = students;
+    }
     
+    public void saveToFile(){
+        try(PrintWriter pw=new PrintWriter(new FileWriter(filename))){
+            for(int i=0;i<students.size();i++){
+                Student stu=students.get(i);
+                pw.println(stu.toString());
+            }
+            JOptionPane.showMessageDialog(null, "FILE SAVED SUCCESSFULLY!");
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null, "ERROR SAVING FILE: "+e.getMessage());
+        }
+    }
+    public Student createStudent(String studentLine){
+        String [] split=studentLine.split(",");
+        return new Student(Integer.parseInt(split[0]),split[1],Integer.parseInt(split[2]),split[3],split[4],Float.parseFloat(split[5]));
+    }
 }
